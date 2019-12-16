4640067dc88a74649c48bf4c126abca4b47daf5c
TheSale <thepavic@gmail.com>
2019-12-16 02:25:03 +0100

    Update THEGAME.py
    updated and working

--- THEGAME.py
+++ THEGAME.py
@@ -6,23 +6,16 @@ func_constant ([1,2,3,4,5,6,7,8,9,10,12,13,14])
 '''
 #userinput = int(input())
 import random
-for x in range(1):
-    print (random.randint(1,10))
+while True:
+  user = input()
+  num = random.randint(1,10)
+  print (num)
 
-user = input()
-
-while random > 5:
-    print ("high")
-while random <= 5:
-    print ("low")
-
-
-while random > 5 and user == "high":
-    print("user win")
-while random > 5 and user == "low":
-    print("user lose")
-while random <= 5 and user == "high":
-    print("user lose")
-while random <= 5 and user == "low":
-    print("user win")
-
+  if num > 5 and user == "high":
+      print("User Win")
+  elif num > 5 and user == "low":
+      print("User Lose")
+  elif num <= 5 and user == "high":
+      print("User Lose")
+  elif num <= 5 and user == "low":
+      print("User Win")
